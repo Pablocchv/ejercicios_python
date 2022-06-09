64846ebf4345a917c89593d6f27091bfd0037154
Pablocchv <79512294+Pablocchv@users.noreply.github.com>
2022-06-09 14:11:19 +0200

    Add files via upload

--- eje4.py
+++ eje4.py
@@ -1,11 +1,14 @@
 #Escribir un programa que lea un entero positivo, n, introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta n.
-suma=0
-num=int(input("Escribe un numero"))
-for i in range (1,num+1):
-    print(i)
-    suma=suma+i
-#suma2=0
-#cont=0
-#while num>0 and cont<=num:
- #   suma2=suma+num
-  #  cont=cont+1
+
+num=input("Escribe un numero")
+if num.isdigit():
+    num=int(num)
+    print (num*(num+1)//2)# para evitar que el programa tarde mucho en ejecutar una operacion , expresarlo con notacion cientifica
+    print(sum(range(0, num)))
+else:
+    print("Datos incorrectos")
+#función sum en pyhton
+
+   #for i in range (1,num+1):
+     #   print(i)
+        #suma=suma+i
